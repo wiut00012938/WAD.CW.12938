a47030dc50431245fecbadea76f5a40b2efda728
wiut00012938 <wiut00012938@gmail.com>
2024-02-19 22:35:41 +0500

    feat: Set up Web Application Solution with API and MVC, Install Required Packages
    Created a Web Application solution comprising back-end (API) and front-end (MVC) projects. Set up necessary project folders and installed the following packages: EntityFrameworkCore, SQLServer, Tools, Cloudinary, AutoMapper, and Newtonsoft.JSON.

---
+++ WAD.WebApplication.12938/GradeTrackerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GradeTrackerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
